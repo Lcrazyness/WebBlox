fe0819676579ba87c075c8c3a933b52547ad4886
Lcrazyness <crazyworking9@gmail.com>
2026-08-27 19:16:32 -0700

    Update and rename Script.js to script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+```json
+{
+  "name": "webblox-backend",
+  "version": "1.0.0",
+  "description": "WebBlox Roblox discovery backend",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "engines": {
+    "node": ">=18"
+  },
+  "dependencies": {
+    "express": "^5.1.0"
+  }
+}
+```
+
